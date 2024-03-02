c4669cc21100b0a04900ba58a43626e4919a2fb6
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-02 10:20:21 +0300

    Names changed

---
+++ Lesson 1/Practice1.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = int(input("Enter your age: "))
+
+print(name + " " + surname + " " + str(age) + " years old")
---
+++ Lesson 1/Practice2.py
@@ -0,0 +1,12 @@
+numberX = int(input("Enter a number: "))
+numberY = int(input("Enter another number: "))
+
+sum = numberX + numberY
+subtraction = numberX - numberY
+multiplication = numberX * numberY
+division = numberX / numberY
+
+print("The sum of " + str(numberX) + " and " + str(numberY) + " is " + str(sum))
+print('The subtraction of ' + str(numberX) + " and " + str(numberY) + " is " + str(subtraction))
+print("The multiplication of " + str(numberX) + " and " + str(numberY) + " is " + str(multiplication))
+print("The division of " + str(numberX) + " and " + str(numberY) + " is " + str(division))
