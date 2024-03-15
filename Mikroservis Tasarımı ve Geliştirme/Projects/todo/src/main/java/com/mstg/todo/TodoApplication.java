d018795c22cb802ac8c7c135e77a8839744d0a6f
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-15 22:06:23 +0300

    Implement All methods

--- Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/TodoApplication.java
+++ Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/TodoApplication.java
@@ -2,12 +2,16 @@ package com.mstg.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
-public class TodoApplication {
+@Service
+@Configuration
 
+public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
+        System.out.println("App started...");
     }
-
 }
