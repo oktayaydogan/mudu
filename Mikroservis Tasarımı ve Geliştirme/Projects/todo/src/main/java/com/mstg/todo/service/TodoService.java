e796037ee2323782aaef9b7c67699da48220761c
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-24 21:25:43 +0300

    Add deleteTodo method to TodoService interface

--- Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/service/TodoService.java
+++ Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/service/TodoService.java
@@ -1,12 +1,14 @@
 package com.mstg.todo.service;
 
 import com.mstg.todo.dto.TodoDto;
+import com.mstg.todo.model.Todo;
 
 import java.util.List;
 
 public interface TodoService {
     List<TodoDto> getAlTodos();
-    TodoDto getTodo(String title);
+    Todo getTodo(String title);
     boolean saveTodo(TodoDto obj);
     boolean updateTodo(TodoDto obj);
+    boolean deleteTodo(TodoDto obj);
 }
