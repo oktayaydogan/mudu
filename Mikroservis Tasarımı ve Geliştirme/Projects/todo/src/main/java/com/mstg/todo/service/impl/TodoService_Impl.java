acdc6bba42dd6a754eaf8198be26005d2610bbc5
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-03-24 21:25:59 +0300

    Refactor TodoService_Impl class to add logging and error handling

--- Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/service/impl/TodoService_Impl.java
+++ Mikroservis Tasarımı ve Geliştirme/Projects/todo/src/main/java/com/mstg/todo/service/impl/TodoService_Impl.java
@@ -1,47 +1,74 @@
 package com.mstg.todo.service.impl;
 
+import com.mstg.todo.controller.TodoController;
 import com.mstg.todo.dto.TodoDto;
 import com.mstg.todo.model.Todo;
 import com.mstg.todo.repository.TodoRepository;
 import com.mstg.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TodoService_Impl implements TodoService {
+    private final Logger _logger = LoggerFactory.getLogger(TodoService_Impl.class);
     private final TodoRepository _todoRepository;
 
     @Override
     public List<TodoDto> getAlTodos() {
-        List<Todo> todos = _todoRepository.findAll();
-        if (!todos.isEmpty()) {
-            return todos.stream()
-                    .map(todo -> TodoDto.builder()
-                            .id(todo.getId())
-                            .title(todo.getTitle())
-                            .detail(todo.getDetail())
-                            .build())
-                    .collect(Collectors.toList());
-        } else {
+        try {
+            List<Todo> todoList = _todoRepository.findAll();
+
+
+            if(!todoList.isEmpty()){
+                _logger.info("Fetching all todos.");
+
+                return todoList.stream()
+                        .map(todo -> TodoDto.builder()
+                                .title(todo.getTitle())
+                                .detail(todo.getDetail())
+                                .completed(todo.isCompleted())
+                                .build())
+                        .collect(Collectors.toList());
+                }
+
+            _logger.info("No todos found.");
             return Collections.emptyList();
+        } catch (Exception e) {
+            _logger.error("An error occurred while fetching the todos. Exception: {}", e.getMessage());
+            throw new RuntimeException("An error occurred while fetching the todos.");
         }
     }
 
     @Override
-    public TodoDto getTodo(String title) {
+    public Todo getTodo(String title) {
         try {
-            Todo todo = _todoRepository.findByTitle(title);
-            return TodoDto.builder()
-                    .id(todo.getId())
-                    .title(todo.getTitle())
-                    .detail(todo.getDetail())
-                    .build();
+            Optional<Todo> optionalTodo = _todoRepository.findByTitle(title);
+
+            if (optionalTodo.isPresent()) {
+                _logger.info("Fetching todo with title: {}", title);
+                Todo todo = optionalTodo.get();
+
+                //return TodoDto.builder()
+                //        .id(todo.getId())
+                //        .title(todo.getTitle())
+                //        .detail(todo.getDetail())
+                //        .build();
+
+                return todo;
+            }
+
+            _logger.info("No todo found with title: {}", title);
+            return null;
         } catch (Exception e) {
+            _logger.error("An error occurred while fetching the todo. Exception: {}", e.getMessage());
             return null;
         }
     }
@@ -54,9 +81,11 @@ public class TodoService_Impl implements TodoService {
                     .detail(obj.getDetail())
                     .build();
 
-            _todoRepository.save(newTodo);
+            Todo savedEntity = _todoRepository.save(newTodo);
+            _logger.info("Todo saved: {}", savedEntity);
             return true;
         } catch (Exception e) {
+            _logger.error("An error occurred while saving the todo. Exception: {}", e.getMessage());
             return false;
         }
     }
@@ -64,15 +93,50 @@ public class TodoService_Impl implements TodoService {
     @Override
     public boolean updateTodo(TodoDto obj) {
         try {
-            Todo todo = _todoRepository.findById(obj.getId()).orElse(null);
+            Todo todo = getTodo(obj.getTitle());
+
             if (todo != null) {
-                todo.setTitle(obj.getTitle());
-                todo.setDetail(obj.getDetail());
+                if (obj.getDetail() == null && !todo.isCompleted()) {
+                    todo.setCompleted(true);
+
+                    _logger.info("Todo completed: {}", todo);
+                } else if (obj.getDetail() != null) {
+                    todo.setDetail(obj.getDetail());
+
+                    _logger.info("Todo detail changed: {}", todo);
+                }
+
+                _logger.info("Object: {}", obj.isCompleted());
+                _logger.info("Todo: {}", todo.isCompleted());
+
                 _todoRepository.save(todo);
+
+                _logger.info("Todo updated: {}", todo);
                 return true;
             }
+
+            _logger.warn("No todo found with title: {}", obj.getTitle());
             return false;
         } catch (Exception e) {
+            _logger.error("An error occurred while updating the todo. Exception: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteTodo(TodoDto obj) {
+        try {
+            Todo todo = getTodo(obj.getTitle());
+
+            if (todo != null) {
+                _todoRepository.delete(todo);
+                _logger.info("Todo deleted: {}", todo);
+                return true;
+            }
+
+            _logger.warn("No todo found with title: {}", obj.getTitle());
+            return false;
+        } catch (Exception e) {
+            _logger.error("An error occurred while deleting the todo. Exception: {}", e.getMessage());
             return false;
         }
     }
